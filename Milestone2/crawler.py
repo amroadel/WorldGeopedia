b7b0820bc07b9ddab5a8ac275407d99d5215d1a3
amradel <amroadel@aucegypt.edu>
2021-11-07 01:15:44 +0200

    Modified single-val attrs

--- Milestone2/crawler.py
+++ Milestone2/crawler.py
@@ -60,24 +60,102 @@ class Country:
         html_data = requests.get(self.url,'parser.html').text
         soupObj = BeautifulSoup(html_data,'html.parser')
         table_rows = (soupObj.find(class_ = "infobox ib-country vcard"))
-        
-        self.country_name = table_rows.find('div', {'class':'fn org country-name'}).text
-        self.calling_code = table_rows.select_one('th:-soup-contains("Calling code") + td').text
-        self.driving_side = table_rows.select_one('th:-soup-contains("Driving side") + td').text
-        self.gov_type = table_rows.select_one('th:-soup-contains("Government") + td').text
-        self.population = (list(table_rows.select_one('th:-soup-contains("estimate") + td').children)[1].text).strip() # revise this
-        self.area = (list(table_rows.select_one('th:-soup-contains("Total") + td').children)[0].text).strip() # revise this
-        self.water_percentage = table_rows.select_one('th:-soup-contains("Water") + td').text
 
-        self.gdp_pp = table_rows.select_one('th:-soup-contains("PPP")').find_parent()
-        self.gdp_pp = (list(self.gdp_pp.find_next_sibling().select_one('td').children)[1].text)
+        country_name = table_rows.find('div', {'class':'fn org country-name'})
+        if country_name is not None:
+            country_name = country_name.text
 
-        self.gdp_nominal = table_rows.select_one('th:-soup-contains("nominal")').find_parent()
-        self.gdp_nominal = (list(self.gdp_nominal.find_next_sibling().select_one('td').children)[1].text)
+        calling_code = table_rows.select_one('th:-soup-contains("Calling code") + td')
+        if calling_code is not None:
+            calling_code = calling_code.text
 
-        self.gini_index = list(table_rows.select_one('th:-soup-contains("Gini") + td').children)[1].text
-        self.hdi = list(table_rows.select_one('th:-soup-contains("HDI") + td').children)[1].text
+        driving_side = table_rows.select_one('th:-soup-contains("Driving side") + td')
+        if driving_side is not None:
+            driving_side = driving_side.text
+
+        gov_type = table_rows.select_one('th:-soup-contains("Government") + td')
+        if gov_type is not None:
+            gov_type = gov_type.text
+
+        population = table_rows.select_one('th:-soup-contains("Population")').find_parent()
+        population = population.find_next_sibling().select_one('td')
+        if population is not None:
+            population = list(population.children)
+            for i in range(len(population)):
+                if population[i].text != '':
+                    population = population[i].text
+                    break
+
+        area = table_rows.select_one('th:-soup-contains("Area")').find_parent()
+        area = area.find_next_sibling().select_one('th:-soup-contains("Total") + td')
+        if area is not None:
+            area = list(area.children)
+            print(area[0].text)
+            for i in range (len(area)):
+                if area[i].text != '':
+                    area = area[i].text
+                    break
+
+        water_percentage = table_rows.select_one('th:-soup-contains("Water") + td')
+        if water_percentage is not None:
+            water_percentage = list(water_percentage.children)
+            for i in range(len(water_percentage)):
+                if water_percentage[i].text != '':
+                    water_percentage = water_percentage[i].text
+                    break
+
+        gdp_pp = table_rows.select_one('th:-soup-contains("PPP")').find_parent()
+        gdp_pp = gdp_pp.find_next_sibling().select_one('th:-soup-contains("Total") + td:-soup-contains("$")')
+        if gdp_pp is not None:
+            gdp_pp = list(gdp_pp.children)
+            for i in range (len(gdp_pp)):
+                if (gdp_pp[i].text.find("$") >= 0):
+                    gdp_pp = gdp_pp[i].text
+                    break
+
+        gdp_nominal = table_rows.select_one('th:-soup-contains("nominal")').find_parent()
+        gdp_nominal = gdp_nominal.find_next_sibling().select_one('th:-soup-contains("Total") + td:-soup-contains("$")')
+        if gdp_nominal is not None:
+            gdp_nominal = list(gdp_nominal.children)
+            for i in range (len(gdp_nominal)):
+                if (gdp_nominal[i].text.find("$") >= 0):
+                    gdp_nominal = gdp_nominal[i].text
+                    break
+
+        gini_index = table_rows.select_one('th:-soup-contains("Gini") + td')
+        if gini_index is not None:
+            gini_index = list(gini_index.children)
+            for i in range (len(gini_index)):
+                if gini_index[i].text != '':
+                    gini_index = gini_index[i].text
+                    break
+
+        hdi = table_rows.select_one('th:-soup-contains("HDI") + td')
+        if hdi is not None:
+            hdi = list(hdi.children)
+            for i in range (len(hdi)):
+                if hdi[i].text != '':
+                    hdi = hdi[i].text
+                    break
 
 
+        self.name = country_name
+        self.calling_code = calling_code
+        self.driving_side = driving_side
+        self.gov_type = gov_type
+        self.population = population
+        self.area = area
+        self.water_percentage = water_percentage
+        self.gdp_pp = gdp_pp
+        self.gdp_nominal = gdp_nominal
+        self.gini_index = gini_index
+        self.hdi = hdi
+
+        # self.covid_cases = -1
+        # self.vaccines = -1
+        # self.timezone = []
+        # self.currency = []
+        # self.legislature = []
+        # self.official_lang = []
 
 
